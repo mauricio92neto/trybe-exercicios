7f55c41f8952147073d465e7068cb079c02352a3
mauricio <demelonetomauricio92@gmail.com>
2022-03-08 10:00:15 -0300

    adiciona exercicios

---
+++ trybe-exercicios-21-b/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-1°-passos/teste.js
@@ -0,0 +1,64 @@
+const a = 15;
+const b = 25;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a % b);
+console.log(a / b);
+
+const d = 20;
+const c = 30;
+if (b > c) {
+    console.log("d é maior que c");
+} else {
+    console.log("c é maior que d");
+
+} 
+ const f = 10;
+ const g = 20;
+ const h = 30;
+if(f > g && f > h) {
+  console.log("O maior é f"); 
+
+  } else if (g > f && g > h ) {
+
+  console.log("O maior é g");
+  } else {
+    console.log("maior é o h");
+
+}
+
+const number = 8;
+if(number > 0 ){
+    console.log('positive');
+} else if(number < 0) {
+    console.log("negativo");
+} else {
+    console.log("zero");
+}
+
+
+
+    
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
